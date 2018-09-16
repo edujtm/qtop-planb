e43a9edcee490101a04d7d2ff25a2722f112e55c
edujtm <eduzemacedo@hotmail.com>
2018-09-15 22:40:56 -0300

    Layout inicial do task manager

---
+++ processinfo.cpp
@@ -0,0 +1,6 @@
+#include "processinfo.h"
+
+ProcessInfo::ProcessInfo()
+{
+
+}
---
+++ processinfo.h
@@ -0,0 +1,11 @@
+#ifndef PROCESSINFO_H
+#define PROCESSINFO_H
+
+
+class ProcessInfo
+{
+public:
+    ProcessInfo();
+};
+
+#endif // PROCESSINFO_H
